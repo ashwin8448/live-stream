fdd7b4a3db356aaab7a24c167b956b5f217513aa
Ashwin Nair <ashwin.nair@qburst.com>
2024-11-21 14:05:18 +0530

    added close room

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,20 @@
-// import { useEffect } from "react";
 import "./App.css";
 import VideoScreener from "./components/VideoScreener/VideoScreener";
 import { useSearchParams } from "react-router-dom";
-
 
 function App() {
   const [searchParams] = useSearchParams();
   const roomName: string | null = searchParams.get("room_name");
   const token: string | null = searchParams.get("token");
 
-  const handleSubmit = () => {
-    const form = document.createElement("form");
-    form.method = "POST";
-    form.action = "https://twilio.kytt.site/create_room"; // The URL to handle the request
-  
-    document.body.appendChild(form); // Append form to DOM
-    form.submit(); // Submit the form
-  };
-  // useEffect(() => {
-  //   (async () => {
-  //     let res =await fetch("https://twilio.kytt.site/create_room", {
-  //       method: "POST",
-  //     });
-
-  //     console.log(res.redirected)
-  //     console.log(res.url)
-      
-  //   })();
-
-  // });
   return (
     <>
       {roomName && token ? (
         <VideoScreener credentials={{ roomName, token }} />
       ) : (
-        <>
-        <h1>Invalid URL</h1>
-        <button onClick={handleSubmit}>Create Room</button>
-        </>
+        <form method="POST" action="https://twilio.kytt.site/create_room">
+          <button type="submit">Create Room</button>
+        </form>
       )}
     </>
   );
--- src/components/VideoScreener/VideoScreener.tsx
+++ src/components/VideoScreener/VideoScreener.tsx
@@ -10,10 +10,12 @@ const VideoScreener = ({
   const videoRef = useRef<HTMLVideoElement>(null);
   const roomRef = useRef<Room | null>(null); // Persist room reference
   const localStreamRef = useRef<MediaStream | null>(null);
-  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [sessionStatus, setSessionStatus] = useState<
+    "not yet started" | "ongoing" | "ended"
+  >("not yet started");
 
   const handleStartStop = async () => {
-    if (!isRecording) {
+    if (sessionStatus === "not yet started") {
       try {
         localStreamRef.current = await navigator.mediaDevices.getUserMedia({
           video: {
@@ -28,7 +30,7 @@ const VideoScreener = ({
         if (videoRef.current) {
           videoRef.current.srcObject = new MediaStream(localVideoStream);
           videoRef.current.play();
-          setIsRecording(true);
+          setSessionStatus("ongoing");
 
           const room = await connect(credentials.token, {
             name: credentials.roomName,
@@ -46,6 +48,7 @@ const VideoScreener = ({
     } else {
       if (localStreamRef.current) {
         localStreamRef.current.getTracks().forEach((track) => {
+          console.log(track);
           track.stop(); // Stop all tracks (audio + video)
         });
       }
@@ -55,25 +58,42 @@ const VideoScreener = ({
         videoRef.current.srcObject = null;
       }
 
-      setIsRecording(false);
+      setSessionStatus("ended");
 
       // Disconnect from the room if it exists
       if (roomRef.current) {
         roomRef.current.disconnect();
         roomRef.current = null; // Reset room reference
+        (() => {
+          fetch("https://twilio.kytt.site/close_room", {
+            method: "POST",
+            body: JSON.stringify({ room_name: credentials.roomName }),
+          });
+        })();
       }
     }
   };
 
   return (
     <div>
-      <h1>Video Screener</h1>
-      <video ref={videoRef} disablePictureInPicture className="videoElement" />
-      <div>
-        <button onClick={handleStartStop}>
-          {isRecording ? "Stop recording" : "Start recording"}
-        </button>
-      </div>
+      {sessionStatus != "ended" ? (
+        <>
+          <h1>Video Screener</h1>
+          <video
+            ref={videoRef}
+            disablePictureInPicture
+            className="videoElement"
+          />
+          <div>
+            <button onClick={handleStartStop}>
+              {sessionStatus === "not yet started" && "Start recording"}
+              {sessionStatus === "ongoing" && "Stop recording"}
+            </button>
+          </div>
+        </>
+      ) : (
+        <h1>Session is over</h1>
+      )}
     </div>
   );
 };
