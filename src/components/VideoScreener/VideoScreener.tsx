ce887427e8206c6adaf33b4181cb37bd2a9ce28a
Ashwin Nair <ashwin.nair@qburst.com>
2024-11-21 17:18:56 +0530

    bug fix

--- src/components/VideoScreener/VideoScreener.tsx
+++ src/components/VideoScreener/VideoScreener.tsx
@@ -1,18 +1,21 @@
 import { useRef, useState } from "react";
 import { connect, Room } from "twilio-video";
 import "./styles.css";
+import { useNavigate } from "react-router-dom";
 
 const VideoScreener = ({
   credentials,
 }: {
   credentials: { roomName: string; token: string };
 }) => {
+  const navigate = useNavigate();
   const videoRef = useRef<HTMLVideoElement>(null);
   const roomRef = useRef<Room | null>(null); // Persist room reference
   const localStreamRef = useRef<MediaStream | null>(null);
   const [sessionStatus, setSessionStatus] = useState<
     "not yet started" | "ongoing" | "ended"
   >("not yet started");
+  const [error, setError] = useState<string>("");
 
   const handleStartStop = async () => {
     if (sessionStatus === "not yet started") {
@@ -42,13 +45,12 @@ const VideoScreener = ({
           roomRef.current = room; // Store room reference in useRef
         }
       } catch (error) {
-        alert("Error accessing webcam and microphone");
-        console.error("Error accessing webcam and microphone: ", error);
+        console.log(error);
+        setError(error.message);
       }
     } else {
       if (localStreamRef.current) {
         localStreamRef.current.getTracks().forEach((track) => {
-          console.log(track);
           track.stop(); // Stop all tracks (audio + video)
         });
       }
@@ -64,20 +66,24 @@ const VideoScreener = ({
       if (roomRef.current) {
         roomRef.current.disconnect();
         roomRef.current = null; // Reset room reference
-        (() => {
-          fetch("https://twilio.kytt.site/close_room", {
+        (async () => {
+          const res = await fetch("https://twilio.kytt.site/close_room", {
             method: "POST",
             body: JSON.stringify({ room_name: credentials.roomName }),
-            credentials: 'include'
+            credentials: "include",
           });
+          console.log(res);
         })();
+        navigate("/", { replace: true });
       }
     }
   };
 
   return (
     <div>
-      {sessionStatus != "ended" ? (
+      {error ? (
+        <h1>{error}</h1>
+      ) : sessionStatus != "ended" ? (
         <>
           <h1>Video Screener</h1>
           <video
